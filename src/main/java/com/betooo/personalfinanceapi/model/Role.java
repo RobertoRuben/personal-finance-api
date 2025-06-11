cbb1cf3ed57e709ac4003642342143767513339d
RobertoRuben <robertoch263@gmail.com>
2025-06-11 17:18:00 -0500

    ✨ feat(roles): add Role entity with JPA annotations

---
+++ src/main/java/com/betooo/personalfinanceapi/model/Role.java
@@ -0,0 +1,37 @@
+package com.betooo.personalfinanceapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "roles")
+
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, unique = true, length = 255)
+    private String roleName;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
