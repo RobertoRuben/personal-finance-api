9d490c185493467e41feb72b045e68c2c0308371
RobertoRuben <robertoch263@gmail.com>
2025-06-11 17:26:09 -0500

    ✨ feat(roles): create RoleRepository for role data access

---
+++ src/main/java/com/betooo/personalfinanceapi/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.betooo.personalfinanceapi.repository;
+
+import com.betooo.personalfinanceapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
