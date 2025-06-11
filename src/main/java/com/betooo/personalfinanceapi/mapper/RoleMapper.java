4cd5482334c767ca36d61f5a58637ebabe9db764
RobertoRuben <robertoch263@gmail.com>
2025-06-11 18:02:49 -0500

    ✨ feat(roles): add RoleMapper for mapping between Role and RoleDTO

---
+++ src/main/java/com/betooo/personalfinanceapi/mapper/RoleMapper.java
@@ -0,0 +1,23 @@
+package com.betooo.personalfinanceapi.mapper;
+
+import com.betooo.personalfinanceapi.dto.RoleDTO;
+import com.betooo.personalfinanceapi.model.Role;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoleMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RoleDTO toDTO(Role role) {
+        return modelMapper.map(role, RoleDTO.class);
+    }
+
+    public Role toEntity(RoleDTO roleDTO) {
+        return modelMapper.map(roleDTO, Role.class);
+    }
+
+}
