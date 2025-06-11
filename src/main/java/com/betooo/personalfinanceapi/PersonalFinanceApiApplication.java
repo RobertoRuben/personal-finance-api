50844cd494fe802aee6d2e0dc15f757cd71d53d4
RobertoRuben <robertoch263@gmail.com>
2025-06-11 17:18:09 -0500

    ✨ feat(env): integrate dotenv for environment variable management

--- src/main/java/com/betooo/personalfinanceapi/PersonalFinanceApiApplication.java
+++ src/main/java/com/betooo/personalfinanceapi/PersonalFinanceApiApplication.java
@@ -1,5 +1,6 @@
 package com.betooo.personalfinanceapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonalFinanceApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(PersonalFinanceApiApplication.class, args);
     }
 
