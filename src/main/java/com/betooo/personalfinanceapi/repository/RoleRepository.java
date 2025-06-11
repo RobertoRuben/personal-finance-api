a43d2fa5c7a97d0a29eabbd78eb8369e947f8959
RobertoRuben <robertoch263@gmail.com>
2025-06-11 17:27:18 -0500

    ✨ feat(roles): add existsByRoleName method to RoleRepository for role existence check

--- src/main/java/com/betooo/personalfinanceapi/repository/RoleRepository.java
+++ src/main/java/com/betooo/personalfinanceapi/repository/RoleRepository.java
@@ -4,4 +4,7 @@ import com.betooo.personalfinanceapi.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    boolean existsByRoleName(Role role);
+
 }
